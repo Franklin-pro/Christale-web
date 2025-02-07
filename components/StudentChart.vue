a58a102829059ef4e09cc1e94bfe4db374988387
Franklin-pro <franklinprogrammer@gmail.com>
2025-02-07 12:40:40 +0200

    studentsCharts:course management

--- components/StudentChart.vue
+++ components/StudentChart.vue
@@ -64,7 +64,28 @@
   </div>
 
   <!-- Radial Chart -->
-  <Doughnut :data="chartData" :options="options" />
+  <div class="bg-white relative px-6 py-4 w-[50%] h-[500px] rounded-lg shadow">
+    <div class="flex items-center justify-between mb-6">
+      <h2 class="text-xl text-gray-800 font-semibold">Analytics</h2>
+    </div>
+    <div class=" h-[400px] flex justify-center">
+      <Doughnut :data="chartData" :options="options" />
+      <div class="absolute bottom-1  w-full flex justify-center gap-4 text-sm text-gray-600">
+        <div class="flex items-center gap-2">
+          <div class="w-3 h-3 rounded-full bg-[#8B5CF6]"></div>
+          <span>Sales (55.5%)</span>
+        </div>
+        <div class="flex items-center gap-2">
+          <div class="w-3 h-3 rounded-full bg-[#10B981]"></div>
+          <span>Distribute (33.5%)</span>
+        </div>
+        <div class="flex items-center gap-2">
+          <div class="w-3 h-3 rounded-full bg-[#1C1C28]"></div>
+          <span>Returns (11%)</span>
+        </div>
+      </div>
+    </div>
+  </div>
   <!-- <v-chart class="chart" :option="option" autoresize /> -->
 
 </div>
